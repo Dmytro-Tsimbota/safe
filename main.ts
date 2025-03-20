8774b8505cd0076c31acb4df04ecf5d7dca7347a
Dmytro-Tsimbota <d31404304@gmail.com>
2025-03-20 23:08:12 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,28 @@
 input.onButtonPressed(Button.A, function () {
     temp = "" + user_key + "A"
+    check_key()
+})
+function check_key () {
     user_key = temp
     serial.writeLine(user_key)
+    if (user_key == key) {
+        servos.P0.setAngle(0)
+    } else if (user_key.length > key.length) {
+        basic.showIcon(IconNames.No)
+        control.reset()
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    servos.P0.setAngle(45)
 })
 input.onButtonPressed(Button.B, function () {
     temp = "" + user_key + "B"
-    user_key = temp
-    serial.writeLine(user_key)
+    check_key()
 })
-// це місце де знаходиця парось від замка
 let temp = ""
 let user_key = ""
+let key = ""
 let secret = 3791
-let key = "BBABA"
+key = "BBABA"
 user_key = ""
+servos.P0.setAngle(45)
