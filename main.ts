675b66ace5e2a82226ade3733046f1fcb65e491b
Dmytro-Tsimbota <d31404304@gmail.com>
2025-03-26 17:10:22 +0200

    An error was found in the code that caused the servo to rotate incorrectly.

--- main.ts
+++ main.ts
@@ -6,14 +6,14 @@ function check_key () {
     user_key = temp
     serial.writeLine(user_key)
     if (user_key == key) {
-        servos.P0.setAngle(0)
+        servos.P0.setAngle(45)
     } else if (user_key.length > key.length) {
         basic.showIcon(IconNames.No)
         control.reset()
     }
 }
 input.onButtonPressed(Button.AB, function () {
-    servos.P0.setAngle(45)
+    servos.P0.setAngle(0)
 })
 input.onButtonPressed(Button.B, function () {
     temp = "" + user_key + "B"
@@ -25,4 +25,4 @@ let key = ""
 let secret = 3791
 key = "BBABA"
 user_key = ""
-servos.P0.setAngle(45)
+servos.P0.setAngle(0)
